ab2a550ccdf86c34234a013a7a27db72acff250d
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-08-18 02:58:19 +0500

    Implemented Auth Context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,30 @@
-import { Stack,Slot } from "expo-router";
+import { AuthContextProvider, useAuth } from "@/context/authContext";
+import { Slot, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
+
+function MainLayout() {
+  const router = useRouter();
+  const { isAuthenticated } = useAuth();
+  const segments = useSegments();
+  useEffect(() => {
+    if (typeof isAuthenticated === "undefined") return;
+    console.log(isAuthenticated)
+    const inApp = segments[0] === "(app)";
+    if (isAuthenticated && !inApp) {
+      router.replace("/home");
+    } else if (isAuthenticated === "false") {
+      router.replace("/log-in");
+    } else {
+      router.replace("/sign-up");
+    }
+  }, [isAuthenticated]);
+  return <Slot />;
+}
 
 export default function RootLayout() {
   return (
-   <Slot/>
+    <AuthContextProvider>
+      <MainLayout />
+    </AuthContextProvider>
   );
 }
