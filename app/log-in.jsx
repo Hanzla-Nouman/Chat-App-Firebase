adb8dda4ba98485e262d78e7795f0b4676ad09ca
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-08-20 18:52:35 +0500

    Upadting login screen

--- app/log-in.jsx
+++ app/log-in.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput, TouchableHighlight } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import {
@@ -22,24 +22,52 @@ export default function logIn() {
             source={require("../assets/images/login.jpg")}
           />
         </View>
-        <View className=" gap-y-4 ">
+        <View className=" gap-y-6 ">
           <Text
             style={{ fontSize: hp(4) }}
             className="font-bold tracking-wide text-center text-neutral-800"
           >
             Sign In
           </Text>
-          <View
-            style={{ height: hp(7),width:wp(90) }}
-            className="flex-row  px-4 text-center bg-neutral-100 items-center   rounded-xl"
-          >
-            <Octicons name="mail" color={"gray"} size={hp(2.7)} />
-          </View>
-          <View
-            style={{ height: hp(7),width:wp(90) }}
-            className="flex-row  px-4 text-center bg-neutral-100 items-center   rounded-xl"
-          >
-            <Octicons name="mail" color={"gray"} size={hp(2.7)} />
+          <View className="gap-y-5">
+            <View
+              style={{ height: hp(7), width: wp(90) }}
+              className="flex-row  px-4 text-center bg-neutral-100 items-center   rounded-xl"
+            >
+              <Octicons name="mail" color={"gray"} size={hp(2.7)} />
+              <TextInput
+                style={{ fontSize: hp(2) }}
+                placeholder="Email Address"
+                className="flex-1 font-semibold text-neutral-700 ml-4"
+              />
+            </View>
+            <View>
+              <View
+                style={{ height: hp(7), width: wp(90) }}
+                className="flex-row  px-4 text-center bg-neutral-100 items-center   rounded-xl"
+              >
+                <Octicons name="lock" color={"gray"} size={hp(2.7)} />
+                <TextInput
+                  style={{ fontSize: hp(2) }}
+                  placeholder="Password"
+                  className="flex-1 font-semibold text-neutral-700 ml-4"
+                />
+              </View>
+              <Text style={{ fontSize: hp(1.7) }} className="text-right font-semibold text-neutral-500">Forgot Password?</Text>
+            </View>
+            <View>
+            <TouchableHighlight className="bg-emerald-500 py-3 rounded-xl">
+              <Text style={{ fontSize: hp(2.8) }} className="text-white text-center font-semibold tracking-wider">Sign In</Text>
+            </TouchableHighlight>
+            <View className="flex-row gap-x-3 justify-center mt-3 ">
+            <Text className="" style={{fontSize: hp(1.8)}}>
+              Don't have an account?
+            </Text>
+            <Text className="" style={{fontSize: hp(1.8)}}>
+              SignUp
+            </Text>
+            </View>
+            </View>
           </View>
         </View>
       </View>
